0b9403b4eccf5ffc28da1f2cb887892fa26b5bdb
Mihika7 <94743689+Mihika7@users.noreply.github.com>
2021-12-29 16:08:30 +0530

    Update bot_alive.py

--- userbot/plugins/assistant/bot_alive.py
+++ userbot/plugins/assistant/bot_alive.py
@@ -12,7 +12,7 @@ currentversion = "2.1"
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Black Lightning"
 ASSIS_PIC = os.environ.get("ASSIS_PIC", None)
 if ASSIS_PIC is None:
-     PM_IMG = "https://telegra.ph/file/beb843ce962a738c52cad.jpg"
+     PM_IMG = "https://telegra.ph/file/fc167c440e22b250b7941.jpg"
 else:
      PM_IMG = ASSIS_PIC
 
@@ -22,9 +22,9 @@ pm_caption += "► **SYSTEMS STATS**\n"
 pm_caption += "► **Telethon Version:** `1.15.0` \n"
 pm_caption += f"► **Assistant Version** : `{currentversion}`\n"
 pm_caption += f"► **My Master** : {DEFAULTUSER} \n"
-pm_caption += "► **License** : [GNU General Public License v3.0](https://github.com/KeinShin/Black-Lightning/blob/master/LICENSE)\n"
-pm_caption += "► **Copyright** : [Black Lighting](GitHub.com/keinshin/Black-Lightning)\n"
-light = [[Button.url("✧Repos✧", "https://github.com/KeinShin/Black-Lightning"), Button.url("✧Support✧", "https://t.me/lightning_support_group")]]
+pm_caption += "► **License** : [GNU General Public License v3.0](https://github.com/Mihika7/Mihika/blob/master/LICENSE)\n"
+pm_caption += "► **Copyright** : [Black Lighting](GitHub.com/Mihika7/Mihika)\n"
+light = [[Button.url("✧Repos✧", "https://github.com/Mihika7/Mihika"), Button.url("✧Support✧", "https://t.me/all_web_series_mihika")]]
 light +=[[custom.Button.inline("✧Help✧", data="gibcmd")]]
 @tgbot.on(events.NewMessage(pattern="^/alive" , func=lambda e: e.sender_id == bot.uid))
 async def _(event):
